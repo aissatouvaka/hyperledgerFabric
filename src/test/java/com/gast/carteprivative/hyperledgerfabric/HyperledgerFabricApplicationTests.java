fdda4655a0534abc9a3de15f02a3f74ed3c27960
vaka <vaka@gmail.com>
2021-06-08 15:36:42 +0100

    importation du sdk Hyperledger fabric

---
+++ src/test/java/com/gast/carteprivative/hyperledgerfabric/HyperledgerFabricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gast.carteprivative.hyperledgerfabric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperledgerFabricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
